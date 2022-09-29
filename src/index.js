e77a8f162ea83b3c11bced647293f2f93fa30c2d
Probip <robertap202@gmail.com>
2022-09-29 16:02:39 +0200

    social, kenaz Tv

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import './scss/main.scss';
 import 'jquery';
 import { event } from 'jquery';
 require('slick-carousel');
-console.log('Heloooooo world!');
 
 $(document).ready(function(){
-    console.log('koristi se jQuery');
     $(".slick-slider.slider1").slick({
         slidesToShow: 1,
         infinite:true,
@@ -13,8 +11,8 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 8000,
         arrows: true,
-       });
-       $(".zoom-slider").slick({
+    });
+    $(".zoom-slider").slick({
         slidesToShow: 7,
         infinite:true,
         slidesToScroll: 1,
@@ -22,28 +20,25 @@ $(document).ready(function(){
         autoplaySpeed: 4000,
         arrows: true,
         //focusOnSelect: true,
-       });
-       $(".zoom-slider-img").click((e)=>{
+    });
+    $(".zoom-slider-img").click((e)=>{
         $(".zoom-slider").find('img.active').removeClass('active');
         const activeImg=$(e.target).attr('src');//ime slike na koju je korisnik kliknuo
         $(".zoom-slider img").each(($i,$img)=>{
             /*slike se pojavljuju više puta u slideru
-            treba svakoj slici koja ima isti src tj. naziv 
-            dodijeliti klasu active */
+            treba svakoj slici koja ima isti src tj. naziv dodijeliti klasu active */
             if($img.getAttribute('src')==activeImg){
-                //console.log('pronađena slika');
                 $img.classList.add('active');
             }
         })
-       });
-       $(".zoom-slider-container").bind('mousemove',function(e){ 
+    });
+    $(".zoom-slider-container").bind('mousemove',function(e){ 
         console.log("e.pageX: "+e.pageX + ", e.pageY: " + e.pageY); 
-});
-       $(".tag-item").click((e)=>{
+    });
+    $(".tag-item").click((e)=>{
         $(".tag-container").find('button.tag-item.active').removeClass('active');
         $(e.target).addClass('active');
-        //ako više tagova može biti aktivno u isto vrijeme:
+        //ako više tagova može biti aktivno u isto vrijeme (umjesto linija 41 i 42):
         //$(e.target).toggleClass('active');
-       });
-    
+    });  
 })
