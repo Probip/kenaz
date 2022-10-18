bc0064568b6477bf15a81061b124d9390d0ef774
Probip <robertap202@gmail.com>
2022-10-18 11:40:03 +0200

    mijenjanje glavnog sadržaja na category.html ovisno o odabranoj kategoriji

--- src/js/index.js
+++ src/js/index.js
@@ -138,14 +138,4 @@ $(document).ready(function(){
             }
         });
     });
-    $('.categories li').click((e)=>{
-        $('.categories').find('li.active').removeClass('active');
-        //$('.categories li').css('display','flex');
-        $(e.target).parent().addClass('active');
-       // $('.categories ul').css('visibility','hidden');
-        //$('.categories li.active').css('display','flex');
-    })
-   /* $('.categories li.active').click(()=>{  
-        $('.categories li').css('display','flex');
-    })*/
 })
