ac135ff6b0a00b45a5410c34ce32561e76149a24
Probip <robertap202@gmail.com>
2022-10-24 11:19:08 +0200

    uveden način rješavanja greške kod učitavanja data.json pomoću getJSON(), dijelovi jtmla koji se generiraju su napisani pomoću template literals radi preglednosti

--- src/js/file.js
+++ src/js/file.js
@@ -4,7 +4,7 @@ var cat='news',news='',name,email,text,d,hours,timeOfTheDay,minutes,date,htmlCom
 
 $(document).ready(function(){
 
-
+    
     //NAČIN 1:
   /*function categoryMainContent(cat){
         news='<div class="news-list-item"><h2>'+cat[0].toUpperCase()+cat.slice(1)+'</h2></div>';
@@ -96,11 +96,37 @@ $(document).ready(function(){
     function categoryMainContent(cat,v){
         news='<div class="news-list-item"><h2>'+cat[0].toUpperCase()+cat.slice(1)+'</h2></div>';
         $(v).each((i,value)=>{
-            news +='<div class="news-list-item"><h2>'+value.title+'</h2>';
-            news +='<div class="row"><p class="date"><img src="./imgs/calendar.svg" alt="kalendar">'+value.date+'</p><p class="date">'+value.author+'</p><p class="date">'+value.comments+' Comments</p></div>';
-            news +='<div class="row-grid"><img src="'+value.img+'" alt="article img"><p>'+value.text+'</p><button class="btn-news"><a href="./single.html">Read article</a></button></div></div><hr class="line"/>';
+            news +=`
+            <div class="news-list-item">
+                <h2>${value.title}</h2>
+                <div class="row">
+                    <p class="date">
+                        <img src="./imgs/calendar.svg" alt="kalendar">${value.date}
+                    </p>
+                    <p class="date">Author: ${value.author}</p>
+                    <p class="date">${value.comments} Comments</p>
+                </div>
+                <div class="row-grid">
+                    <img src="${value.img}" alt="article img">
+                    <p>${value.text}</p>
+                    <button class="btn-news"><a href="./single.html">Read article</a></button>
+                </div>
+            </div>
+            <hr class="line"/>`;
         })
-        news +='<div class="page-number row"><div class="number">1</div><div class="number">2</div><div class="number">3</div><div class="number active">4</div><div class="number">5</div><div class="number">6</div><div class="number">7</div><div class="number">8</div><div class="number">9</div><div class="number">10</div></div>';
+        news +=`
+        <div class="page-number row">
+            <div class="number">1</div>
+            <div class="number">2</div>
+            <div class="number">3</div>
+            <div class="number active">4</div>
+            <div class="number">5</div>
+            <div class="number">6</div>
+            <div class="number">7</div>
+            <div class="number">8</div>
+            <div class="number">9</div>
+            <div class="number">10</div>
+        </div>`;
         return news;
     }//dodan parametar v
 
@@ -114,18 +140,46 @@ $(document).ready(function(){
                 }
             })
         })
+        .done(function() {
+            console.log( "Uspješno učitani podaci iz data.json!");
+        })
+        .fail(function(err) {
+            console.log( `error: ${err.status}`);
+        })
+        .always(function() {
+            console.log( "Završeno" );
+        });
     })//novo
 if(cat=='news'){
     $.getJSON("../data/data.json",(obj)=>{
         var value=obj[cat];
-        console.log(cat)
         $('#category-main-content').html(categoryMainContent(cat,value));
     })
+    .done(function() {
+        console.log( "Uspješno učitani podaci iz data.json za kategoriju news!" );
+    })
+    .fail(function(err) {
+        console.log( `error: ${err.status}`);
+    })
+    .always(function() {
+        console.log( "Završeno" );
+    });
 }
     function createComment(name,email,text,date,hours) {
-        htmlComment='<div class="row pt-45 m-50-30-0-30"><img src="./imgs/user1.png" alt="user image"><span><div class="row space-between p-0"><div class="row p-0"><p class="user p-0">';
-        htmlComment+=name+'</p><p class="date p-0">'+date+' '+hours+'</p></div><a class="reply">Reply</a></div>';
-        htmlComment+='<p class="p-0 text-comment">'+text+'</p></span></div>';
+        htmlComment=`
+        <div class="row pt-45 m-50-30-0-30">
+            <img src="./imgs/user1.png" alt="user image">
+            <span>
+                <div class="row space-between p-0">
+                    <div class="row p-0">
+                        <p class="user p-0">${name}</p>
+                        <p class="date p-0">${date} ${hours}</p>
+                    </div>
+                    <a class="reply">Reply</a>
+                </div>
+                <p class="p-0 text-comment">${text}</p>
+            </span>
+        </div>`;
         return htmlComment;
     }//isto
 
@@ -133,7 +187,7 @@ if(cat=='news'){
     const month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
     $('#submit-comment').click((e)=>{
         e.preventDefault();
-        name=$('form #name').val();
+        user=$('form #name').val();
         email=$('form #email').val();
         text=$('form #text').val();
         if((name!='')&&(email!='')&&text!=''){
@@ -152,20 +206,20 @@ if(cat=='news'){
             if(minutes<10){
                 minutes='0'+minutes;
             }
-            hours=hours+':'+minutes+' '+timeOfTheDay;
-            date=month[d.getMonth()]+' '+d.getDate()+','+d.getFullYear();
-            console.log(name,email,text,date,hours);
+            hours=`${hours}:${minutes} ${timeOfTheDay}`;
+            date=`${month[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
+            console.log(user,email,text,date,hours);
             //do tu je isto
 
             $.getJSON("../data/data.json",(obj)=>{
                 $.each(obj, (key,val)=>{
                     if(key=='comments'){
                         console.log(key,val);
-                        val.push({"name":name,"email":email,"text":text,"date":date,"hours":hours});
+                        val.push({"name":user,"email":email,"text":text,"date":date,"hours":hours});
                     } 
                 })
             })//novo
-            $('#comments h2:last-of-type').before(createComment(name,email,text,date,hours));     
+            $('#comments h2:last-of-type').before(createComment(user,email,text,date,hours));     
         }
         else{
             alert('Molimo upišite sve podatke.');
